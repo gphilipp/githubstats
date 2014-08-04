bd3ccd99af36522210bf34340fef69e6fb33e3e5
Gilles Philippart <gilles.philippart@gmail.com>
2014-08-04 20:46:09 +0200

    It works

--- src/githubstats/core.cljs
+++ src/githubstats/core.cljs
@@ -1,15 +1,34 @@
 (ns githubstats.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [weasel.repl :as ws-repl]))
+            [weasel.repl :as ws-repl]
+            [goog.net.XhrIo :as xhr]
+            [cljs.core.async :as async :refer [close! chan >! <! put! take!]])
+  (:require-macros [cljs.core.async.macros :refer [go alt!]]))
 
 (enable-console-print!)
 (ws-repl/connect "ws://localhost:9001" :verbose true)
 
 (def app-state (atom {:text "Hello world!"}))
 
+(def repos "https://api.github.com/users/gphilipp/repos")
+
+
+(defn GET [url]
+  (let [c (chan)]
+    (xhr/send url
+              (fn [e]
+                (go
+                  (>! c (-> e .-target .getResponseJson))
+                  (close! c))))
+    c))
+
+(def res (GET repos))
+(go (prn (<! res)))
+
+
 (om/root
   (fn [app owner]
     (dom/h1 nil (:text app)))
   app-state
-  {:target (. js/document (getElementById "app"))})
+  {:target (. js/document (getElementById "stats"))})
